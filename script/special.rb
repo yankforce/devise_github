07da2ba6602a7177f8995cdc1c3c4a5270859481
Ken <ken@seeice.com>
2013-06-30 10:19:04 -0400

    Update special.rb

--- script/special.rb
+++ script/special.rb
@@ -9,6 +9,7 @@ git filter-branch --commit-filter '
         fi' HEAD
 
 
+git filter-branch -f --env-filter "GIT_AUTHOR_DATE='Sun Jun 30 06:14:56 2013 -0400'; GIT_COMMITTER_DATE='Sun Jun 30 06:14:56 2013 -0400'; GIT_AUTHOR_NAME='Ken Lu'; GIT_COMMITTER_NAME='Ken Lu'; GIT_AUTHOR_EMAIL='ken@seeice.com'; GIT_COMMITTER_EMAIL='ken@seeice.com'" HEAD
 
 git filter-branch -f --env-filter "GIT_AUTHOR_DATE='Sat Jun 29 06:14:56 2013 -0400'; GIT_COMMITTER_DATE='Sat Jun 29 06:14:56 2013 -0400';" HEAD  
 
